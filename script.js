457f10ef6663d7789e3ec3d13553a0f61f808702
SHERIN JASORIA <sherinjasoria.23@gmail.com>
2026-01-08 03:58:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,89 +1,101 @@
-const video = document.getElementById("video")
+const video = document.getElementById("video");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const moodDisplay = document.getElementById("moodDisplay");
+const emotionMessage = document.getElementById("emotionMessage");
+const historyText = document.getElementById("history");
+const dominantMoodText = document.getElementById("dominantMood");
 
-// IMPORTANT: GitHub Pages safe path
-const MODEL_URL = "models"
+const MODEL_URL = "models";
 
-// Load models
+let stream, canvas, interval;
+let emotionHistory = [];
+
+const moodColors = {
+  happy: "#fff7cc",
+  sad: "#dbeafe",
+  angry: "#fee2e2",
+  surprised: "#ede9fe",
+  fearful: "#e5e7eb",
+  disgusted: "#dcfce7",
+  neutral: "#ffffff"
+};
+
+const moodMessages = {
+  happy: "You look cheerful today 😊",
+  sad: "It’s okay to take a break 💙",
+  angry: "Take a deep breath 😌",
+  surprised: "That looks exciting 😲",
+  fearful: "You’re safe here 🤍",
+  disgusted: "Let’s refresh your mood 🌿",
+  neutral: "You look calm and composed 🙂"
+};
+
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
-    faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL)
-]).then(startVideo)
+  faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+  faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL)
+]);
 
-// Start webcam
-function startVideo() {
-    navigator.mediaDevices.getUserMedia({ video: true })
-        .then(stream => {
-            video.srcObject = stream
-        })
-        .catch(err => {
-            console.error("Camera error:", err)
-        })
-}
+startBtn.onclick = () => {
+  navigator.mediaDevices.getUserMedia({ video: true }).then(s => {
+    stream = s;
+    video.srcObject = stream;
+  });
+};
 
-// When video plays
+stopBtn.onclick = () => {
+  if (stream) stream.getTracks().forEach(t => t.stop());
+  clearInterval(interval);
+  moodDisplay.innerText = "Mood: Camera stopped";
+};
+
 video.addEventListener("play", () => {
+  if (canvas) canvas.remove();
+  canvas = faceapi.createCanvasFromMedia(video);
+  document.querySelector(".video-wrapper").append(canvas);
 
-    const canvas = faceapi.createCanvasFromMedia(video)
+  const size = {
+    width: video.getBoundingClientRect().width,
+    height: video.getBoundingClientRect().height
+  };
 
-    // Wrap video + canvas
-    const container = document.createElement("div")
-    container.style.position = "relative"
-    container.style.display = "inline-block"
+  canvas.width = size.width;
+  canvas.height = size.height;
+  faceapi.matchDimensions(canvas, size);
 
-    video.parentNode.insertBefore(container, video)
-    container.appendChild(video)
-    container.appendChild(canvas)
+  interval = setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceExpressions();
 
-    // Match displayed size
-    const displaySize = {
-        width: video.getBoundingClientRect().width,
-        height: video.getBoundingClientRect().height
+    const resized = faceapi.resizeResults(detections, size);
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    faceapi.draw.drawDetections(canvas, resized);
+
+    if (resized.length > 0) {
+      const expressions = resized[0].expressions;
+      const mood = Object.keys(expressions).reduce((a, b) =>
+        expressions[a] > expressions[b] ? a : b
+      );
+
+      emotionHistory.push(mood);
+      if (emotionHistory.length > 10) emotionHistory.shift();
+
+      historyText.innerText = emotionHistory.join(" → ");
+
+      const freq = {};
+      emotionHistory.forEach(e => freq[e] = (freq[e] || 0) + 1);
+      const dominant = Object.keys(freq).reduce((a, b) =>
+        freq[a] > freq[b] ? a : b
+      );
+
+      dominantMoodText.innerText = dominant.toUpperCase();
+
+      moodDisplay.innerText = `Mood: ${mood.toUpperCase()}`;
+      emotionMessage.innerText = moodMessages[mood];
+      document.body.style.background = moodColors[mood];
     }
-
-    canvas.width = displaySize.width
-    canvas.height = displaySize.height
-    canvas.style.position = "absolute"
-    canvas.style.top = "0"
-    canvas.style.left = "0"
-    canvas.style.pointerEvents = "none"
-
-    faceapi.matchDimensions(canvas, displaySize)
-
-    setInterval(async () => {
-        const detections = await faceapi
-            .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-            .withFaceExpressions()
-
-        const resizedDetections =
-            faceapi.resizeResults(detections, displaySize)
-
-        const ctx = canvas.getContext("2d")
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-        // Draw face box
-        faceapi.draw.drawDetections(canvas, resizedDetections)
-
-        // Draw expression label
-        resizedDetections.forEach(detection => {
-            const expressions = detection.expressions
-
-            const maxExpression = Object.keys(expressions).reduce((a, b) =>
-                expressions[a] > expressions[b] ? a : b
-            )
-
-            const confidence =
-                Math.round(expressions[maxExpression] * 100)
-
-            const label =
-                `${maxExpression.toUpperCase()} (${confidence}%)`
-
-            const drawBox = new faceapi.draw.DrawBox(
-                detection.detection.box,
-                { label }
-            )
-
-            drawBox.draw(canvas)
-        })
-
-    }, 300)
-})
+  }, 400);
+});
